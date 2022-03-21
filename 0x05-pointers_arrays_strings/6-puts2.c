2869227a8f41619f0a8c2d823b850db724f95166
mesoma <anigbogumesoma1@gmail.com>
2022-03-21 09:15:54 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,25 @@
 #include "main.h"
 /**
- * puts2 - Prints out every other char of the string.
- * @str: input string to print.
+ * puts2 - prints one character out of two, starting with the first
+ *
+ * @str: the string to look at
+ *
+ * Return: nothing
  */
+
 void puts2(char *str)
 {
 	int i;
+	int len;
 
-	while (str[i] != '\0')
+	i = 0;
+	len = _strlen(str) - 1;
+
+	while (i <= len)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		_putchar(str[i]);
+		i += 2;
 	}
+
 	_putchar('\n');
 }
