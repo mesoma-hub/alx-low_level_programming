e10f5bab0921d0e898abe0b9b2a7c1c667b3d2b7
mesoma <anigbogumesoma1@gmail.com>
2022-05-02 10:34:21 -0700

    initial commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+
